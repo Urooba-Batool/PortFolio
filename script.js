951ea4f3be4b255b500c5577fb772fa54ded12ae
Urooba Batool <162785493+Urooba-Batool@users.noreply.github.com>
2025-07-24 06:03:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// Typing effect
-const phrases = ["Software Engineer", "Frontend Developer", "C# & Java Developer"];
+//Typing effect
+const phrases = ["Software Engineer", "Frontend Learner", "C# & Java Developer"];
 let index = 0, i = 0, current = "";
 function type() {
   if (i < phrases[index].length) {
@@ -15,7 +15,7 @@ function type() {
 }
 type();
 
-// Dark Mode Toggle
+//Dark Mode Toggle
 function toggleMode() {
   document.body.classList.toggle("dark-mode");
   const btn = document.querySelector(".toggle-btn");
@@ -25,3 +25,34 @@ function toggleMode() {
     btn.textContent = "🌙 Dark Mode";
   }
 }
+
+//COntact form Validations
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const form = e.target;
+  const status = document.getElementById("form-status");
+
+  //Client-side validation
+  if (!form.name.value || !form.email.value || !form.message.value) {
+    status.textContent = "Please fill all fields.";
+    status.style.color = "red";
+    return;
+  }
+
+  //Email format validation
+  const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+  if (!form.email.value.match(emailPattern)) {
+    status.textContent = "Please enter a valid email.";
+    status.style.color = "red";
+    return;
+  }
+
+  //Successful submission
+  status.textContent = "Message sent successfully!";
+  status.style.color = "green";
+  form.reset();
+});
+
+
+AOS.init();
